db86b2ffc06eaa769bb03e7e081bd94669fa7ed6
Turla <Eliseo.Turla@ingrammicro.com>
2018-08-16 11:31:02 +0800

    get ntid of visitor

--- app/helper.php
+++ app/helper.php
@@ -2,9 +2,9 @@
 
 function getUser()
 {
-    // $ntid = $_SERVER["AUTH_USER"];
-    // $ntid = substr($ntid, strrpos($ntid, "\\") + 1);
-    $ntid = 'usture00';
+    $ntid = $_SERVER["AUTH_USER"];
+    $ntid = substr($ntid, strrpos($ntid, "\\") + 1);
+    // $ntid = 'usture00';
     // $ntid = 'usbav00';
     return $ntid;
 }
